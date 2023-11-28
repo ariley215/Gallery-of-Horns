6e2816a35e2860cf6ea30ac8c2241a19eb5d782f
Andrea Riley <andreariley@me.com>
2023-11-28 16:12:11 -0500

    clean up

---
+++ src/components/HornedBeast.jsx
@@ -0,0 +1,12 @@
+
+const HornedBeast = ({title, imageUrl, description}) => {
+  return (
+  <div>
+    <h2>{title}</h2>
+    <img src={imageUrl} alt={title}/>
+    <p>{description}</p>
+  </div>
+  );
+};
+
+export default HornedBeast
